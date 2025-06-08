cd43ea70ab9ce923b8e40f31549bba3b96a6dbfd
ArnoSahakyan <sahakyan.arno@gmail.com>
2025-06-08 17:50:50 +0400

    simplifying type and formatting

--- src/api/account.ts
+++ src/api/account.ts
@@ -1,7 +1,7 @@
 import { api } from './axios.ts';
+import { PersonalFormData } from '../shared';
 
-export const updatePersonalInfo = (data: { full_name: string; email: string; phone_number?: string }) =>
-  api.put('/account/info', data);
+export const updatePersonalInfo = (data: PersonalFormData) => api.put('/account/info', data);
 
 export const changePassword = (data: { current_password: string; new_password: string }) =>
   api.put('/account/password', data);
